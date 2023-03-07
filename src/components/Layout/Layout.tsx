80a4869a57c0af5a2c7049fbc3fb523b23011bd5
Tarun yadav <56935638+tarunyadav1@users.noreply.github.com>
2023-03-07 11:40:57 +0530

    🔧: meta data

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -23,6 +23,38 @@ const Layout = ({ children }: React.PropsWithChildren<{}>) => {
 		<>
 			<Head>
 				<title>DIVA Donate</title>
+				<meta
+					name="description"
+					content="A novel donation model enabled by blockchain technology
+							Powered by DIVA Protocol"
+				/>
+
+				{/* <!-- Facebook Meta Tags --> */}
+				<meta property="og:url" content="https://www.divadonate.xyz" />
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="DIVA Donate" />
+				<meta
+					property="og:description"
+					content="A novel donation model enabled by blockchain technology
+						Powered by DIVA Protocol"
+				/>
+				<meta
+					property="og:image"
+					content="https://www.divaprotocol.io/_next/image?url=%2Fimages%2Fposts%2Fdiva_donate_header.png&w=3840&q=75"
+				/>
+
+				{/* <!-- Twitter Meta Tags --> */}
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content="DIVA Donate" />
+				<meta
+					name="twitter:description"
+					content="A novel donation model enabled by blockchain technology
+							Powered by DIVA Protocol"
+				/>
+				<meta
+					name="twitter:image"
+					content="https://www.divaprotocol.io/_next/image?url=%2Fimages%2Fposts%2Fdiva_donate_header.png&w=3840&q=75"
+				/>
 			</Head>
 			<div>
 				<NavBar />
