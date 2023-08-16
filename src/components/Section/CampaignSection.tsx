5cf67a0a93c1e68c80c618a31cc669fe5b63bfbc
Walodja1987 <wladimir.weinbender@gmail.com>
2023-08-16 08:54:08 +0200

    add to metamask logic extended to account for multiple pools in a campaign + minor code restructuring

--- src/components/Section/CampaignSection.tsx
+++ src/components/Section/CampaignSection.tsx
@@ -10,6 +10,7 @@ import { Text, Progress, ProgressLabel } from '@chakra-ui/react'
 import { fetchToken, getContract } from '@wagmi/core'
 import { useConnectModal } from '@rainbow-me/rainbowkit'
 import pools from '../../../config/pools.json' // @todo remove when migrated to campaigns.json
+import AddToMetamaskIcon from '../AddToMetamaskIcon'
 import campaigns from '../../../config/campaigns.json'
 import {getShortenedAddress} from "../../utils/general";
 import { divaContractAddressOld, divaContractAddress } from "../../constants"; // @todo remove when migrated to campaigns.json
@@ -72,6 +73,34 @@ export const CampaignSection = () => {
 			...prev,
 			[campaignId]: percentage,
 		}))
+	}
+
+	const handleAddToMetamask = async (campaign: any) => {
+			const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+			
+			for (const pool of campaign.pools) {
+				const token = new ethers.Contract(pool.positionToken, ERC20ABI, provider.getSigner())
+				const decimal = await token.decimals()
+				const symbol = await token.symbol()													
+			
+				try {
+					await (window as any).ethereum.request({
+						method: 'wallet_watchAsset',
+						params: {
+							type: 'ERC20',
+							options: {
+								address: pool.positionToken,
+								symbol: symbol,
+								decimals: decimal,
+								image:
+									'https://res.cloudinary.com/dphrdrgmd/image/upload/v1641730802/image_vanmig.png',
+							},
+						} as any,
+					})
+				} catch (error) {
+					console.error('Error in HandleAddMetaMask', error)
+				}
+			}														
 	}
 
 	useEffect(() => {
@@ -206,61 +235,10 @@ export const CampaignSection = () => {
 									<div className="p-5">
 										<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-[#042940]">
 											{campaign.title}
-										</h5>
-
-										<div
-											onClick={
-												async () => {
-													const provider = new ethers.providers.Web3Provider((window as any).ethereum)
-													const token = new ethers.Contract(campaign.pools[0].positionToken, ERC20ABI, provider.getSigner()) // @todo index hard-coded to 0. Change that.
-													const decimal = await token.decimals()
-													const symbol = await token.symbol()
-
-													try {
-														await (window as any).ethereum.request({
-															method: 'wallet_watchAsset',
-															params: {
-																type: 'ERC20',
-																options: {
-																	address: campaign.pools[0].positionToken,
-																	symbol: symbol,
-																	decimals: decimal,
-																	image:
-																		'https://res.cloudinary.com/dphrdrgmd/image/upload/v1641730802/image_vanmig.png',
-																},
-															} as any,
-														})
-													} catch (error) {
-														console.error('Error in HandleAddMetaMask', error)
-													}
-												}
-											}
-											className="text-indigo-600 flex items-center dark:text-indigo-400">
-											<span className="text-slate-400 font-normal">#{getShortenedAddress(campaign.pools[0].poolId)}</span>
-											{/* @todo: Updated index which is currently hard-coded at 0 */}
-											<button>
-												<svg
-													width="16"
-													height="16"
-													viewBox="0 0 16 16"
-													fill="none"
-													xmlns="http://www.w3.org/2000/svg"
-													className="ml-2">
-													<g>
-														<path
-															d="M8 0C6.41775 0 4.87103 0.469192 3.55544 1.34824C2.23985 2.22729 1.21447 3.47672 0.608967 4.93853C0.00346629 6.40034 -0.15496 8.00888 0.153721 9.56072C0.462403 11.1126 1.22433 12.538 2.34315 13.6569C3.46197 14.7757 4.88743 15.5376 6.43928 15.8463C7.99113 16.155 9.59966 15.9965 11.0615 15.391C12.5233 14.7855 13.7727 13.7602 14.6518 12.4446C15.5308 11.129 16 9.58225 16 8C15.9977 5.87897 15.1541 3.84547 13.6543 2.34568C12.1545 0.845886 10.121 0.00229405 8 0V0ZM10.6667 8.66667H8.66667V10.6667C8.66667 10.8435 8.59643 11.013 8.47141 11.1381C8.34638 11.2631 8.17682 11.3333 8 11.3333C7.82319 11.3333 7.65362 11.2631 7.5286 11.1381C7.40358 11.013 7.33334 10.8435 7.33334 10.6667V8.66667H5.33334C5.15653 8.66667 4.98696 8.59643 4.86193 8.47141C4.73691 8.34638 4.66667 8.17681 4.66667 8C4.66667 7.82319 4.73691 7.65362 4.86193 7.5286C4.98696 7.40357 5.15653 7.33333 5.33334 7.33333H7.33334V5.33333C7.33334 5.15652 7.40358 4.98695 7.5286 4.86193C7.65362 4.73691 7.82319 4.66667 8 4.66667C8.17682 4.66667 8.34638 4.73691 8.47141 4.86193C8.59643 4.98695 8.66667 5.15652 8.66667 5.33333V7.33333H10.6667C10.8435 7.33333 11.0131 7.40357 11.1381 7.5286C11.2631 7.65362 11.3333 7.82319 11.3333 8C11.3333 8.17681 11.2631 8.34638 11.1381 8.47141C11.0131 8.59643 10.8435 8.66667 10.6667 8.66667Z"
-															fill="#898989"
-														/>
-													</g>
-													<defs>
-														<clipPath id="clip0_270_567">
-															<rect width="16" height="16" fill="white" />
-														</clipPath>
-													</defs>
-												</svg>
+											<button onClick={() => handleAddToMetamask(campaign)}>
+												<AddToMetamaskIcon />
 											</button>
-										</div>
-
+										</h5>
 										<div className=" h-[100px] mb-5 border-b-2 border-[#9FC131]">
 											<p className="mb-3 font-normal text-[#000000]">
 												{campaign.desc}
