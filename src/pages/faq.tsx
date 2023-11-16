afc458817aa84aad3c1ea6db5afced9768122827
Walodja1987 <wladimir.weinbender@gmail.com>
2023-11-16 23:41:06 +0100

    FAQ header added on FAQ page

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -8,6 +8,12 @@ export default function Faq() {
 		<main className="relative">
 			<Layout>
 				<div className="justify-center px-4  lg:yx-auto  pt-16 md:pt-[10rem] pb-[21rem] bg-[#F3FDF8]">
+				<div className="pb-10 flex flex-col items-center justify-center">
+					<h1 className="font-lora text-4xl md:text-[60px] text-center lg:text-left">
+						FAQ
+					</h1>
+					<hr className="w-48 h-[8px] mx-auto bg-[#9FC131] border-0 rounded-[20px] mt-5" />
+				</div>
 					<FAQContent />
 				</div>
 			</Layout>
