fb13bcf7a2a5162636cf2a2e483e31342aedb55c
Walodja1987 <37043174+Walodja1987@users.noreply.github.com>
2024-07-31 15:00:31 +0200

    Fine-tuning of Navbar layout

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -17,11 +17,11 @@ export default function NavBar() {
 	return (
 		<>
 			<Sidebar isOpen={isOpen} onClose={onClose} />
-			<nav className="px-4 md:px-6 sm:px-4 py-6 w-full bg-[#F3FDF8] border-b-2 border-[#D6D58E] fixed top-0 left-0 z-20">
-				<div className="mx-auto lg:max-w-screen-2xl flex items-center md:px-0 justify-between">
-					<div className="flex gap-2 max-w-fit items-center outline-none py-2 md:py-0 lg:block">
+			<nav className="px-4 md:px-6 sm:px-4 w-full bg-[#F3FDF8] border-b-2 border-[#D6D58E] fixed top-0 left-0 z-20">
+				<div className="mx-auto lg:max-w-screen-2xl flex items-center justify-between">
+					<div className="flex gap-2 max-w-fit items-center outline-none py-3 xl:py-[23px] xl:block">
 						<button
-							className=" rounded-md text-gray-700 outline-none lg:hidden md:p-3 hover:bg-slate-200 w-6 md:w-14"
+							className="rounded-md text-gray-700 outline-none xl:hidden md:p-3 hover:bg-slate-200 w-6 md:w-14"
 							onClick={onOpen}>
 							{<RxHamburgerMenu size={'auto'} />}
 						</button>
@@ -32,7 +32,7 @@ export default function NavBar() {
 					</div>
 
 					<div
-						className={`flex-1 justify-self-center pb-3 mt-8 md:pb-0 md:mt-0 hidden lg:block`}>
+						className={`flex-1 justify-self-center pb-3 mt-8 md:pb-0 md:mt-0 hidden xl:block`}>
 						<NavbarLinks activePath={pathname} />
 					</div>
 
